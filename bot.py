25a246a29e877356abe8ba30a61b2ce26d56020d
Owen Thomson <OwenRThomson@gmail.com>
2022-02-19 18:05:58 +0000

    Sending broken code to Andrew LMAO

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,10 +15,17 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content.startswith('$artin'):
+        await message.channel.send('Fuck off!')
+    if message.content.startswith('$quote'):
+        data = []
+        async for msg in message.channel.history(limit=100):
+            if msg.author != client.user:
+                data = data.append(msg.content)
+            if len(data) == 100:
+                break
+        print(data)                      
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
