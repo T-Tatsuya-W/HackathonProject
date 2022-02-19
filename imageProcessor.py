1c5b3cb73e3728bdb1471c69896682ae2c2e58d0
Toby Tatsuya Watkinson <tobytw312@gmail.com>
2022-02-19 17:37:28 +0000

    img processing for both os

--- imageProcessor.py
+++ imageProcessor.py
@@ -6,25 +6,38 @@ img = Image.open(src1)
 src2 = "sources/text.txt"
 text = open(src2, "r")
 
+try:
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+    
+    # Custom font style and font size
+    #myFont = ImageFont.load_default()
+    fontSize = 65
+    fontType = "arial"              #"arial" for Windows, "Arial" for Mac
+    myFont = ImageFont.truetype(fontType, fontSize)
+    
+    textPosition = (20, 40) # (x, y) from top left
+    textContents = "Nice Car"
+    fontColour = (225,0,225) #rgb colour values
+    I1.text(textPosition, textContents, font=myFont, fill =fontColour)
+except OSError:
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+    
+    # Custom font style and font size
+    #myFont = ImageFont.load_default()
+    fontSize = 65
+    fontType = "Arial"              #"arial" for Windows, "Arial" for Mac
+    myFont = ImageFont.truetype(fontType, fontSize)
+    
+    textPosition = (20, 40) # (x, y) from top left
+    textContents = "Nice Car"
+    fontColour = (225,0,225) #rgb colour values
+    I1.text(textPosition, textContents, font=myFont, fill =fontColour)
 
-
- 
-# Call draw Method to add 2D graphics in an image
-I1 = ImageDraw.Draw(img)
- 
-# Custom font style and font size
-#myFont = ImageFont.load_default()
-fontSize = 65
-fontType = "arial"              #"arial" for Windows, "Arial" for Mac
-myFont = ImageFont.truetype(fontType, fontSize)
- 
-textPosition = (20, 40) # (x, y) from top left
-textContents = "Nice Car"
-fontColour = (225,0,225) #rgb colour values
-I1.text(textPosition, textContents, font=myFont, fill =fontColour)
- 
+    
 
 img.show()  #temporarily shows the created image
- 
+
 img.save("car2.png")    # Save the image
 
