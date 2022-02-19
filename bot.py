ff174f33f23c512fe001b6638938b9ca32683b56
Owen Thomson <OwenRThomson@gmail.com>
2022-02-19 21:49:41 +0000

    Finished bot backend (mostly)

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 # bot.py
+#region Imports
 from asyncio.windows_events import NULL
 import os
-
 import discord
 from dotenv import load_dotenv
 import pandas as pd
+import random as rnd
+#endregion
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,27 +16,38 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    #Checks if the message author is the bot to stop it from commanding itself
     if message.author == client.user:
         return
+    #Fuck you artin
     if message.content.startswith('$artin'):
         await message.channel.send('Fuck off!')
-    if message.content.startswith('$quote'):
+    
+    #Generates quote
+    if message.content.startswith('$quote'): #checks a user has called the command
+        #Declares new lis
         messagecontent = []
         messageauthor = []
 
+        #Loops through the message history
         async for msg in message.channel.history(limit=100):
             if msg.author != client.user:
-                #print("the current message is------"+msg.content)
                 if type(msg.content) == list:
-                    #print("has reached the end of the messages")
                     break
+                elif len(msg.content) < 1 or len(msg.content) > 25 or msg.content.startswith('$') == True:
+                    continue
                 else:
                     messagecontent.append(msg.content)
-                    messageauthor.append(msg.author)
-        print(messagecontent)                      
+                    messageauthor.append(msg.author.name)
 
+        x = rnd.randrange(len(messagecontent)-1)
+        print(messagecontent[x] + "\n" + messageauthor[x])
+                       
+
+#region OnReadyEvent
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+#endregion
 
 client.run(TOKEN)
