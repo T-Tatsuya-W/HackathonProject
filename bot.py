08768aa686932fa3e892181ef3107340f98c9f11
Andrew Li <andrewfs2003@gmail.com>
2022-02-20 07:06:30 +0000

    Finished again

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import random as rnd
 #image processor for image handling
 from PIL import Image,ImageDraw,ImageFont
 import imageProcessor as IP
-import Construcor/constructor_preqoute as CS
+from Constructor import constructor_preqoute as CS
 
 #endregion
 
