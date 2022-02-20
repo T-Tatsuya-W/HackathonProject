02db484153c3ade92c73a6bd543ca99c78a7e941
Artin Yarahmadi <artiniyar@gmail.com>
2022-02-20 11:51:34 +0000

    alteryx

---
+++ Constructor/constructror_preqoute.py
@@ -0,0 +1,20 @@
+import pandas as pd
+def generator():
+    from random import randint
+
+    df = pd.read_csv("Constructor/DataSet_UniqueIDs.csv")
+    N = len(df)
+    row = df.iloc[randint(0, N-1)] 
+
+    name = row["Name"]
+    _id = row["ID"]
+    sport = row["Sport"]
+    age = row["Age"]
+    team = row["Team"]
+    medal = row["Medal"]
+    year = row["Year"]
+
+
+    print(age)
+    afterQuote = f"{name} \n{age} years old {medal} medalist in {sport} from {team} "
+    return afterQuote
